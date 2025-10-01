9fed178fe4145ae927dfacc9511001414ea3fdca
Diogo <diogosf2006@gmail.com>
2025-10-01 20:53:16 -0300

    adicionado Tarefas service, controller, client e dto e modificado os dtos

---
+++ src/main/java/com/diogo/bffagendadortarefas/business/TarefasService.java
@@ -0,0 +1,48 @@
+
+package com.diogo.bffagendadortarefas.business;
+
+
+import com.diogo.bffagendadortarefas.business.dto.in.TarefasDTORequest;
+import com.diogo.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
+import com.diogo.bffagendadortarefas.infrastructure.client.TarefasClient;
+import com.diogo.bffagendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TarefasService {
+
+    private final TarefasClient tarefasClient;
+
+    public TarefasDTOResponse gravarTarefa(String token, TarefasDTORequest dto){
+        return tarefasClient.gravarTarefas(dto, token);
+    }
+
+    public List<TarefasDTOResponse> buscaTarefasAgendadasPorPeriodo(LocalDateTime datainicial,
+                                                                    LocalDateTime datafinal, String token){
+        return tarefasClient.buscaListadeTarefasPorPeriodo(datainicial, datafinal, token);
+    }
+
+
+     public List<TarefasDTOResponse> buscaTarefasPorEmail(String token){
+        return tarefasClient.buscaTarefasPorEmail(token);
+    }
+
+    public void deletaTarefaPorId(String id, String token){
+      tarefasClient.deletaTarefaPorId(id, token);
+    }
+
+    public TarefasDTOResponse alteraStatus(StatusNotificacaoEnum status, String id, String token){
+        return tarefasClient.alteraStatusNotificacao(status, id,token);
+    }
+
+    public TarefasDTOResponse updateTarefas(TarefasDTORequest dto, String id, String token){
+        return tarefasClient.updateTarefas(dto, id, token);
+    }
+
+
+}
---
+++ src/main/java/com/diogo/bffagendadortarefas/infrastructure/enums/StatusNotificacaoEnum.java
@@ -0,0 +1,5 @@
+package com.diogo.bffagendadortarefas.infrastructure.enums;
+
+public enum StatusNotificacaoEnum {
+    PENDENTE, NOTIFICADO, CANCELADO
+}
